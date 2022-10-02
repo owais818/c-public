67b55e346ed46aa91fac071a01db91cb19ee22ec
Syed Ahmed <syedahmed@Syeds-MacBook-Pro.local>
2022-10-02 18:48:56 -0500

    secd

--- hello.c
+++ hello.c
@@ -1,3 +1,8 @@
+/************************************************************\ 
+ *  Author : Deepfreez 					    *	
+ *  Date : 10-2-2022					    *
+ *  Description :  Hello World			            *
+\************************************************************/
 #include <stdio.h>
 
 int main( int argc, char ** argv)
