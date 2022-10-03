e1ab30aad3ace8cad0b47cfe3142fb23e0737bf1
owais818 <owais818@gmail.com>
2022-10-03 00:41:34 -0500

    Update fact.c
    Added Comments

--- fact.c
+++ fact.c
@@ -1,14 +1,20 @@
+/* Sample for recursion */
+
 #include <stdio.h>
+
+/* function prototype */
 
 long  fact ( long  n );
 
+/* Driver Program */
 int main(void)
 {
 	long  n = 10;
 	printf ("%ld \n", fact (n) );
- return 0;
+ 	return 0;
 }
 
+/* Function Definition */
 long  fact ( long n)
 {
 	return ( (n == 0 || n == 1) ? 1 : (n * fact (n -1 ) ) ) ;
